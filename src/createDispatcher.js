30effdeafa5d12821a96987b22e922c1ef586301
Phil Plückthun <phil@plckthn.me>
2015-11-30 20:37:19 +0100

    Turn Dispatcher to extension of Observable
    Instead of returning an object with our methods, containing
    an attribute named stream that contains the actual AnonymousObservable,
    let's extend the AnonymousObservable with our Dispatcher method
    to be able to use the Dispatcher directly as an Observable.

--- src/createDispatcher.js
+++ src/createDispatcher.js
@@ -4,51 +4,54 @@ import {
 } from 'rx'
 
 export default function createDispatcher() {
-  const dispatcher = ReplaySubject()
-  dispatcher.onNext(null)
+  const dispatcher = new ReplaySubject()
+  dispatcher.onNext(null) // Initialisation action
 
   const identifier = Symbol()
   const cache = []
 
-  return {
-    dispatch(action) {
-      if (typeof action === 'function') {
-        return Promise.resolve(
-          action(res => {
-            dispatcher.onNext(res)
-          })
-        ).then(res => {
+  // Extend Observable with our Dispatcher methods
+  const res = dispatcher.asObservable()
+  const proto = Object.create(Object.getPrototypeOf(res))
+  Object.setPrototypeOf(res, proto)
+
+  proto.dispatch = action => {
+    if (typeof action === 'function') {
+      return Promise.resolve(
+        action(res => {
           dispatcher.onNext(res)
         })
-      }
+      ).then(res => {
+        dispatcher.onNext(res)
+      })
+    }
 
-      dispatcher.onNext(action)
-    },
-
-    getState(fn) {
-      if (typeof fn[identifier] === 'number')
-        return cache[fn[identifier]].getValue()
-
-      throw `Function wasn't yet reduced and is therefore unknown.`
-    },
-
-    reduce(fn, init = null) {
-      if (typeof fn[identifier] === 'number')
-        return cache[fn[identifier]]
-
-      const store = BehaviorSubject(init)
-
-      dispatcher
-        .scan(fn)
-        .distinctUntilChanged()
-        .subscribe(store.onNext)
-
-      fn[identifier] = cache.length
-      cache.push(store)
-
-      return store.asObservable()
-    },
-
-    stream: dispatcher.asObservable()
+    dispatcher.onNext(action)
   }
+
+  proto.getState = fn => {
+    if (typeof fn[identifier] === 'number')
+      return cache[fn[identifier]].getValue()
+
+    throw `Function wasn't yet reduced and is therefore unknown.`
+  }
+
+  proto.reduce =(fn, init = null) => {
+    if (typeof fn[identifier] === 'number')
+      return cache[fn[identifier]]
+
+    const store = new BehaviorSubject(init)
+
+    dispatcher
+      .scan(fn)
+      .distinctUntilChanged()
+      .subscribe(store.onNext)
+
+    fn[identifier] = cache.length
+    cache.push(store)
+
+    return store.asObservable()
+  }
+
+  return res
 }
