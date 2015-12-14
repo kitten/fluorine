1d798211dc9577049d8e7ffa08ee989821d02416
Phil Plückthun <phil@plckthn.me>
2015-12-14 14:47:21 +0100

    Patch action promises

--- src/createDispatcher.js
+++ src/createDispatcher.js
@@ -23,7 +23,9 @@ export default function createDispatcher() {
           dispatcher.onNext(res)
         })
       ).then(res => {
-        dispatcher.onNext(res)
+        if (res && res.type) {
+          dispatcher.onNext(res)
+        }
         return res
       })
     }
