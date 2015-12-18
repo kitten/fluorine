ac0fe4b3c73ef0c17f03bdd02c3f8eb433e92395
Phil Plückthun <phil@plckthn.me>
2015-12-18 01:34:48 +0100

    Simplify dispatcher factory
    This slightly reduces the complexity of the dispatcher factory, by
    removing the broken inheritance. Before the prototype was manually swapped
    out which is confusing and slow.
    
    Creating a new object by composition (AnonymousObservable + Dispatcher methods)
    should be faster and easier to read.
    
    Furthermore the setPrototypeOf method is experimental and not widely supported.
    Thus this commit should improve the compatibility to older browsers.

--- src/createDispatcher.js
+++ src/createDispatcher.js
@@ -11,54 +11,50 @@ export default function createDispatcher() {
   const cache = []
   const state = []
 
-  // Extend Observable with our Dispatcher methods
-  const res = dispatcher.asObservable()
-  const proto = Object.create(Object.getPrototypeOf(res))
-  Object.setPrototypeOf(res, proto)
-
-  proto.dispatch = action => {
-    if (typeof action === 'function') {
-      return Promise.resolve(
-        action(res => {
-          dispatcher.onNext(res)
+  return Object.assign(dispatcher.asObservable(), {
+    dispatch(action) {
+      if (typeof action === 'function') {
+        return Promise.resolve(
+          action(res => {
+            dispatcher.onNext(res)
+          })
+        ).then(res => {
+          if (res && res.type) {
+            dispatcher.onNext(res)
+          }
+          return res
         })
-      ).then(res => {
-        if (res && res.type) {
-          dispatcher.onNext(res)
-        }
-        return res
-      })
+      }
+
+      dispatcher.onNext(action)
+      return Promise.resolve(action)
+    },
+    getState(fn) {
+      if (typeof fn[identifier] === 'number')
+        return state[fn[identifier]]()
+
+      throw `Function wasn't yet reduced and is therefore unknown.`
+    },
+    reduce(fn, init = null) {
+      if (typeof fn[identifier] === 'number')
+        return cache[fn[identifier]]
+
+      const store = new BehaviorSubject(init)
+      const anon = store.asObservable()
+
+      dispatcher
+        .scan(fn, null)
+        .distinctUntilChanged()
+        .subscribe(x => store.onNext(x),
+          err => {
+            throw err
+          })
+
+      fn[identifier] = cache.length
+      state.push(store.getValue.bind(store))
+      cache.push(anon)
+
+      return anon
     }
-
-    dispatcher.onNext(action)
-    return Promise.resolve(action)
-  }
-
-  proto.getState = fn => {
-    if (typeof fn[identifier] === 'number')
-      return state[fn[identifier]]()
-
-    throw `Function wasn't yet reduced and is therefore unknown.`
-  }
-
-  proto.reduce = (fn, init = null) => {
-    if (typeof fn[identifier] === 'number')
-      return cache[fn[identifier]]
-
-    const store = new BehaviorSubject(init)
-    const anon = store.asObservable()
-
-    dispatcher
-      .scan(fn, null)
-      .distinctUntilChanged()
-      .subscribe(x => store.onNext(x))
-
-    fn[identifier] = cache.length
-    state.push(store.getValue.bind(store))
-    cache.push(anon)
-
-    return anon
-  }
-
-  return res
+  })
 }
