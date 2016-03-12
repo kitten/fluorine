2b174d0b88a9e8911ff56bb566473219647b8179
Phil Plückthun <phil@plckthn.me>
2016-03-12 01:50:25 +0100

    Change withStore to use connect

--- src/withStore.js
+++ src/withStore.js
@@ -1,70 +1,11 @@
-import React, { Component } from 'react'
-import assert from './util/assert'
-import {
-  Observable
-} from '@reactivex/rxjs'
+import connect from './connect'
 
+// The withStore decorator is deprecated and will be deleted in favor
+// for the connect decorator soon
 export default function withStore(store, prop = 'data') {
-  return Child => class StoreContainer extends Component {
-    constructor(props) {
-      super(props)
-
-      if (typeof store === 'function') {
-        this.store = store(props)
-      } else {
-        this.store = store
-      }
-
-      assert(this.store instanceof Observable, 'Expected `store` to be an Observable.')
-
-      this.state = {
-        data: undefined
-      }
-    }
-
-    subscribe() {
-      if (this.sub) {
-        this.sub.unsubscribe()
-      }
-
-      this.sub = this.store.subscribe(next => {
-        this.setState({
-          data: next
-        })
-      }, err => {
-        throw err
-      })
-    }
-
-    componentWillMount() {
-      this.subscribe()
-    }
-
-    componentWillReceiveProps(newProps) {
-      if (typeof store === 'function') {
-        const newStore = store(newProps)
-
-        if (newStore !== this.store) {
-          this.store = newStore
-          this.subscribe()
-        }
-      }
-    }
-
-    componentWillUnmount() {
-      this.sub.unsubscribe()
-    }
-
-    render() {
-      if (this.state.data === undefined) {
-        return null
-      }
-
-      return (
-        <Child {...this.props} {...{
-          [prop]: this.state.data
-        }}/>
-      )
-    }
+  if (typeof store === 'function') {
+    return connect((_, props) => store(props), prop)
   }
+
+  return connect(store, prop)
 }
