ccbdea18092c1c98585477fb9a6b80f36634b8a5
Phil Plückthun <phil@plckthn.me>
2016-03-17 04:24:13 +0800

    Fix reduce tests

--- test/dispatcher/reduce.js
+++ test/dispatcher/reduce.js
@@ -22,9 +22,10 @@ function CounterStore(state = 0, action) {
 const add = { type: 'ADD' }
 const subtract = { type: 'SUBTRACT' }
 
-test('reduces correctly', t => {
-  const dispatcher = createDispatcher()
+test.cb('reduces correctly', t => {
+  t.plan(1)
 
+  const dispatcher = createDispatcher()
   dispatcher
     .reduce(CounterStore)
     .bufferCount(4)
@@ -32,50 +33,36 @@ test('reduces correctly', t => {
     .subscribe(x => {
       t.same(x, [ 0, 1, 2, 3 ])
     }, err => {
-      t.fail()
+      t.fail(err)
     }, () => {
       t.end()
     })
 
-  dispatcher.dispatch({ type: 'NOISE' }) // This should be ignored
-  dispatcher.dispatch(add)
-  dispatcher.dispatch(add)
-  dispatcher.dispatch(add)
+  dispatcher.next({ type: 'NOISE' }) // This should be ignored
+  dispatcher.next(add)
+  dispatcher.next(add)
+  dispatcher.next(add)
 })
 
-test('reduces stores that emit predictable results', t => {
-  const dispatcher = createDispatcher()
+test.cb('reduces stores that emit predictable results', t => {
+  t.plan(4)
 
+  const dispatcher = createDispatcher()
   Observable.zip(
     dispatcher.reduce(CounterStore),
     dispatcher.reduce(CounterStore)
   )
     .take(4)
-    .filter(([ a, b ]) => a === b)
-    .subscribe(x => {
-      t.pass()
+    .subscribe(([ a, b ]) => {
+      t.is(a, b)
     }, err => {
-      t.fail()
+      t.fail(err)
     }, () => {
       t.end()
     })
 
-  dispatcher.dispatch({ type: 'NOISE' }) // This should be ignored
-  dispatcher.dispatch(add)
-  dispatcher.dispatch(add)
-  dispatcher.dispatch(add)
-})
-
-test('returns predictable references', t => {
-  t.plan(3)
-
-  const dispatcher = createDispatcher()
-
-  const fnA = function (x) { return x }
-  const fnB = function (x) { return x }
-
-  t.is(dispatcher.reduce(fnA), dispatcher.reduce(fnA))
-  t.is(dispatcher.reduce(fnB), dispatcher.reduce(fnB))
-  t.not(dispatcher.reduce(fnA), dispatcher.reduce(fnB))
+  dispatcher.next(add)
+  dispatcher.next(add)
+  dispatcher.next(add)
 })
 
