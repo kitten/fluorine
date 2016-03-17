f901fecf42df054406212985f2efc7884379cb6c
Phil Plückthun <phil@plckthn.me>
2016-03-17 22:09:35 +0800

    Keep withStore un-deprecated

--- src/withStore.js
+++ src/withStore.js
@@ -1,11 +1,6 @@
 import connectStore from './connectStore'
-import warn from './util/warn'
-
-const _withStoreNotice = warn('The `withStore` decorator is deprecated. Please use `connect` instead.')
 
 export default function withStore(store, prop = 'data') {
-  _withStoreNotice()
-
   if (typeof store === 'function') {
     return connectStore((_, props) => store(props), prop)
   }
