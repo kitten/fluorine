b407f03641125d79e33f55c4dddf971a048421a9
Phil Plückthun <phil@plckthn.me>
2016-03-18 11:52:01 +0800

    Fix Provider test

--- test/Provider.js
+++ test/Provider.js
@@ -4,16 +4,15 @@ import React, { Component } from 'react'
 import { Observable } from '@reactivex/rxjs'
 import { mount } from 'enzyme'
 
-import connect from '../lib/connect'
+import connectStore from '../lib/connectStore'
 import Provider from '../lib/Provider'
-import createDispatcher from '../lib/createDispatcher'
 
 function Child({data}) {
   return <div>{data}</div>
 }
 
 test('passes the observable down to connect', t => {
-  const Tester = connect(observable => observable.map(x => `${x} world`))(Child)
+  const Tester = connectStore(observable => observable.map(x => `${x} world`))(Child)
 
   const wrapper = mount(
     <Provider observable={Observable.of('hello')}>
@@ -23,28 +22,5 @@ test('passes the observable down to connect', t => {
 
   const testerWrapper = wrapper.find(Tester).first()
   t.is(testerWrapper.text(), 'hello world')
-})
-
-test('connect passes dispatch, schedule and next', t => {
-  t.plan(6)
-  const dispatcher = createDispatcher()
-
-  const Tester = connect(() => Observable.of('static'))(props => {
-    t.is(typeof props.schedule, 'function')
-    t.is(typeof props.dispatch, 'function')
-    t.is(typeof props.next, 'function')
-
-    t.is(props.schedule, dispatcher.schedule)
-    t.is(props.dispatch, dispatcher.dispatch)
-    t.is(props.next, dispatcher.next)
-
-    return <div>{props.data}</div>
-  })
-
-  const wrapper = mount(
-    <Provider observable={dispatcher}>
-      <Tester/>
-    </Provider>
-  )
 })
 
