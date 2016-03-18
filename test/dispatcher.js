243fd06ce270a0dc806bc0f2eb7e9fe594a6bd08
Phil Plückthun <phil@plckthn.me>
2016-03-18 10:14:06 +0800

    Fix dispatcher test

--- test/dispatcher.js
+++ test/dispatcher.js
@@ -6,47 +6,41 @@ import isObservable from '../lib/util/isObservable'
 
 const action = { type: 'Test' }
 
-test('is an Observable emitting agendas', t => {
+test.cb('is an Observable emitting agendas', t => {
   const dispatcher = createDispatcher()
-
-  dispatcher
-    .first()
-    .every(x => isObservable(x))
-    .subscribe(x => {
-      t.ok(x)
-    })
 
   dispatcher
     .first()
     .mergeAll()
     .subscribe(x => {
       t.is(x, action)
+    }, err => {
+      t.fail(err)
+    }, () => {
+      t.end()
     })
 
-  dispatcher.dispatch(action)
+  dispatcher.next(action)
 })
 
-test('is an Observer taking actions, thunks, promises and agendas', t => {
+test.cb('is an Observer taking actions, thunks, promises and agendas', t => {
   const dispatcher = createDispatcher()
 
   dispatcher
-    .take(4)
-    .every(x => isObservable(x))
-    .subscribe(x => {
-      t.ok(x)
-    })
-
-  dispatcher
-    .take(4)
     .mergeAll()
+    .take(4)
     .subscribe(x => {
       t.is(x, action)
+    }, err => {
+      t.fail(err)
+    }, () => {
+      t.end()
     })
 
   Observable
     .of(
       action, // action
-      dispatch => dispatch(action), // thunk
+      next => { next(action) }, // thunk
       Promise.resolve(action), // promise
       Observable.of(action) // agenda
     ).subscribe(dispatcher.next)
