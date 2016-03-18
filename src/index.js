f73cc503d68d66bfc476022da5a5490868fc1085
Phil Plückthun <phil@plckthn.me>
2016-03-18 11:59:49 +0800

    Export Provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import withActions from './withActions'
 import withStore from './withStore'
 import connectStore from './connectStore'
 import wrapActions from './util/wrapActions'
+import Provider from './Provider'
 
 export {
   createDispatcher,
   withActions,
   withStore,
   connectStore,
-  wrapActions
+  wrapActions,
+  Provider
 }
