bd148ea0d4154494dcb6edd50ae50eaf21bcdeb0
Phil Plückthun <phil@plckthn.me>
2016-03-31 17:36:10 +0200

    Add connectActions decorator
    A decorator that is identical to withActions, but connects
    to the Provider and uses its observer.

---
+++ test/connectActions.js
@@ -0,0 +1,37 @@
+import test from 'ava'
+
+import React, { Component } from 'react'
+import { Observable } from '@reactivex/rxjs'
+import { mount } from 'enzyme'
+
+import connectActions from '../lib/connectActions'
+import Provider from '../lib/Provider'
+import createDispatcher from '../lib/createDispatcher'
+
+const doSomething = () => ({
+  type: 'DO_SOMETHING'
+})
+
+class Child extends Component {
+  render() {
+    return <div/>
+  }
+}
+
+test('passes wrapped actions in props', t => {
+  const dispatcher = createDispatcher()
+  const Tester = connectActions({
+    doSomething
+  })(Child)
+
+  const wrapper = mount(
+    <Provider dispatcher={dispatcher}>
+      <Tester/>
+    </Provider>
+  )
+
+  const child = wrapper.find(Child).first()
+  t.same(Object.keys(child.prop('actions')), [ 'doSomething' ])
+  t.is(typeof child.prop('actions').doSomething, 'function')
+})
+
