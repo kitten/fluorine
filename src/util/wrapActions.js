c1bd160ec75fbaf2da9dbd43f47a8ea0dd1e0986
Phil Plückthun <phil@plckthn.me>
2016-04-04 15:03:22 +0100

    Add arg to wrapActions to make it work recursively

--- src/util/wrapActions.js
+++ src/util/wrapActions.js
@@ -1,34 +1,47 @@
 import assert from './assert'
 
-export default function wrapActions(observer, arg) {
+export default function wrapActions(observer, arg, wrapRecursively = false) {
   assert(typeof observer === 'object' && typeof observer.next === 'function',
     'Expected observer to be an object containing at least a next method.')
 
   const transform = fn => (...args) => observer.next(fn(...args))
+  const wrap = input => {
+    if (typeof input === 'function') {
+      return transform(input)
+    }
 
-  if (typeof arg === 'function') {
-    return transform(arg)
+    if (Array.isArray(input)) {
+      return input.map(x => {
+
+        if (wrapRecursively) {
+          wrap(x);
+        }
+
+        assert(typeof x === 'function', 'Expected array to contain exclusively functions. Did you intend to use wrapActions recursively?')
+        return transform(x)
+      })
+    }
+
+    if (typeof input === 'object') {
+      return Object.keys(input).reduce((prev, key) => {
+        if (input.hasOwnProperty(key)) {
+          const x = input[key]
+
+          if (wrapRecursively) {
+            prev[key] = wrap(x)
+          }
+
+          assert(typeof x === 'function', 'Expected object to contain exclusively functions.')
+          prev[key] = transform(x)
+        }
+
+        return prev
+      }, {})
+    }
+
+    throw new Error('Expected either an action creator or an array/object containing some as arguments.')
   }
 
-  if (Array.isArray(arg)) {
-    return arg.map(fn => {
-      assert(typeof fn === 'function', 'Expected array to contain exclusively functions.')
-      return transform(fn)
-    })
-  }
-
-  if (typeof arg === 'object') {
-    return Object.keys(arg).reduce((prev, key) => {
-      if (arg.hasOwnProperty(key)) {
-        const fn = arg[key]
-        assert(typeof fn === 'function', 'Expected object to contain exclusively functions.')
-        prev[key] = transform(fn)
-      }
-
-      return prev
-    }, {})
-  }
-
-  throw new Error('Expected either an action creator or an array/object containing some as arguments.')
+  return wrap(arg)
 }
 
