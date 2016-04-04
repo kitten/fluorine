cf6d2fbd08903771c17ee55033a7c4b7e199f72b
Phil Plückthun <phil@plckthn.me>
2016-04-04 15:08:45 +0100

    Add wrapRecursively argument to withActions and connectActions

--- src/withActions.js
+++ src/withActions.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import wrapActions from './util/wrapActions'
 
-export default function withActions(observer, actions, prop = 'actions') {
+export default function withActions(observer, actions, prop = 'actions', wrapRecursively = true) {
   return Child => class ActionContainer extends Component {
     render() {
       return (
         <Child {...this.props} {...{
-          [prop]: wrapActions(observer, actions)
+          [prop]: wrapActions(observer, actions, wrapRecursively)
         }}/>
       )
     }
