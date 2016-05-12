ef780fecac6d7cb2428208fe29f0c6a231258b7c
Phil Plückthun <phil@piemapping.com>
2016-05-12 10:43:37 +0100

    Combine stores and reducers in combineReducers

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -1,10 +1,18 @@
 import { Observable } from '@reactivex/rxjs'
+import isObservable from './util/isObservable'
 
 export default function combineReducers(reducers) {
   const keys = Object.keys(reducers)
 
   return reduce => Observable.combineLatest(
-    ...keys.map(key => reduce(reducers[key])),
+    ...keys.map(key => {
+      const reducer = reducers[key]
+      if (isObservable(reducer)) {
+        return reducer
+      }
+
+      return reduce(reducer)
+    }),
     (...values) => values.reduce((acc, val, index) => {
       acc[keys[index]] = val
       return acc
