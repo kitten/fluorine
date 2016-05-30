5ae515a9db0043a560791c7c0ecf8e73f6dfb338
Phil Plückthun <phil@piemapping.com>
2016-05-30 19:11:13 +0100

    Add thunk middleware to counter

--- examples/counter/src/dispatcher.js
+++ examples/counter/src/dispatcher.js
@@ -1,8 +1,9 @@
 import { createDispatcher } from 'fluorine-lib'
+import thunk from 'fluorine-lib/lib/middleware/thunk'
 
 const dispatcher = createDispatcher({
   logging: true
-})
+}, thunk)
 
 export default dispatcher
 
